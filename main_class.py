4240901c987a785d0a8f72fc3c5dd0344aba4eb2
Amin-Bajelan <m.amin.bajelan@gmail.com>
2024-08-12 00:55:39 +0330

    try another way

--- main_class.py
+++ main_class.py
@@ -4,40 +4,46 @@ import random
 back_color = "#021526"
 my_line_color = "#6EACDA"
 action_color = "#E2E2B6"
+turn = "X"
+
+
+def insert_symbol1():
+    buttom1.config(text=select)
+
+
+def insert_symbol9():
+    buttom9.config(text="X", bg=action_color, font=12)
+
 
 window = Tk()
-window.geometry('500x500')
+
 window.title('Tic Tac Toe')
-# window.config(bg=back_color)
-canvas1 = Canvas(window, width=500, height=500, background=back_color, highlightthickness=0)
-canvas1.create_line(80, 190, 400, 190, fill=my_line_color, width=7)  #create for x line
-canvas1.create_line(80, 290, 400, 290, fill=my_line_color, width=7)
+window.minsize(584, 700)
 
-canvas1.create_line(190, 100, 190, 400, fill=my_line_color, width=7)  #create for y line
-canvas1.create_line(290, 100, 290, 400, fill=my_line_color, width=7)
+window.config(bg=back_color)
 
-place1 = [135, 155]
-place2 = [240, 155]
-place3 = [345, 155]
-place4 = [135, 255]
-place5 = [240, 255]
-place6 = [345, 255]
-place7 = [135, 360]
-place8 = [240, 360]
-place9 = [345, 360]
+buttom1 = Button(text="", width=25, height=10, bg=my_line_color)
+buttom1.place(x=11, y=11)
+buttom2 = Button(text="", width=25, height=10, bg=my_line_color)
+buttom2.place(x=200, y=11)
+buttom3 = Button(text="", width=25, height=10, bg=my_line_color)
+buttom3.place(x=389, y=11)
 
-player_turn = random.randint(1, 2)
-if player_turn == 1:
-    print("First player is: *")
-else:
-    print("Second player is: |")
+buttom4 = Button(text="", width=25, height=10, bg=my_line_color)
+buttom4.place(x=11, y=177)
+buttom5 = Button(text="", width=25, height=10, bg=my_line_color)
+buttom5.place(x=200, y=177)
+buttom6 = Button(text="", width=25, height=10, bg=my_line_color)
+buttom6.place(x=389, y=177)
 
-
-for i in range(1,10):
-
-    print(i)
-
-
-canvas1.place(x=0, y=0)
+buttom7 = Button(text="", width=25, height=10, bg=my_line_color)
+buttom7.place(x=11, y=345)
+buttom8 = Button(text="", width=25, height=10, bg=my_line_color)
+buttom8.place(x=200, y=345)
+buttom9 = Button(text="", width=25, height=10, bg=my_line_color, command=insert_symbol9)
+buttom9.place(x=389, y=345)
+canvas = Canvas(window,width= 557, height= 60, bg=my_line_color)
 
 window.mainloop()
+canvas.create_text(275, 30, text=f"{turn}'s turn", fill="black" ,font=12)
+canvas.place(x=11, y=520)
